10e8887bf8c65df66d718e065b5ac4c2c314a225
Lewis <charlotte.rose.lewis.04@gmail.com>
2024-01-07 08:14:26 +0100

    feature : home page done

--- views/PhotoFolio/assets/js/main.js
+++
@@ -1,9 +0,0 @@
-document.querySelectorAll('.load-page').forEach(link => {
-    link.addEventListener('click', function (){
-        fetch(link.dataset.htmlTarget)
-            .then(result => result.text())
-            .then(htmlContent => {
-                document.getElementById('main').innerHTML = htmlContent;
-            })
-    })
-})
